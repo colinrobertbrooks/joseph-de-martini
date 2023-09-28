74a27f2b6ff9d1d74731f1c72e59332089c78353
Colin Brooks <colinrobertbrooks@gmail.com>
2023-09-28 14:40:19 -0400

    refactor: view

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,17 +3,17 @@ import backgroundImg from "../assets/images/poet.min.png";
 import Signature from "./Signature";
 
 const App = () => (
-  <div className="h-[100svh] relative flex justify-center items-center ">
+  <div className="relative h-[100svh] flex justify-center items-center ">
     <img
       className="absolute h-full w-full object-cover object-center lg:object-top"
       src={backgroundImg}
       alt="The Poet"
     />
     <div className="absolute h-full w-full bg-gradient-to-b from-[rgba(45,40,35,0.1)] to-[rgba(45,40,35,0.2)]" />
-    <Signature className="absolute top-[12vh] w-[92vw] m:w-[72vw] max-w-[1200px] h-auto text-[#fad895]">
+    <Signature className="absolute top-[12svh] h-auto max-h-[50svh] w-[92svw] m:w-[72svw] max-w-[1200px] text-[#fad895]">
       <title>Joseph De Martini</title>
     </Signature>
-    <div className="absolute bottom-[4vh] flex gap-4">
+    <div className="absolute bottom-[4svh] flex gap-4">
       <a
         className="text-[#fad895] hover:text-[#fffbf5] focus:text-[#fffbf5]"
         href="https://www.facebook.com/josephdemartiniart"
