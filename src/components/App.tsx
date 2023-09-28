f0df7e52f786db87f406443253dfa9d147c28880
Colin Brooks <colinrobertbrooks@gmail.com>
2023-09-28 14:27:02 -0400

    refactor: view

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,13 +3,13 @@ import backgroundImg from "../assets/images/poet.min.png";
 import Signature from "./Signature";
 
 const App = () => (
-  <div className="h-screen relative flex justify-center items-center ">
+  <div className="h-[100svh] relative flex justify-center items-center ">
     <img
       className="absolute h-full w-full object-cover object-center lg:object-top"
       src={backgroundImg}
       alt="The Poet"
     />
-    <div className="absolute inset-0 h-[100vh] w-full bg-gradient-to-b from-[rgba(45,40,35,0.1)] to-[rgba(45,40,35,0.2)]"></div>
+    <div className="absolute h-full w-full bg-gradient-to-b from-[rgba(45,40,35,0.1)] to-[rgba(45,40,35,0.2)]" />
     <Signature className="absolute top-[12vh] w-[92vw] m:w-[72vw] max-w-[1200px] h-auto text-[#fad895]">
       <title>Joseph De Martini</title>
     </Signature>
