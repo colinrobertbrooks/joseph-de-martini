0d8f173ff57712ceb66d94e33a7907964971fb9f
Colin Brooks <colinrobertbrooks@gmail.com>
2023-09-28 11:35:54 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# [Joseph De Martini](https://www.josephdemartini.art/)
+
+## Setup
+
+`$ npm install`
+
+## Development
+
+`$ npm run dev`
+
+## Build
+
+`$ npm run build`
+
+## Start
+
+`$ npx serve dist`
+
+## Boilerplate
+
+A single-page React application built with [Vite](https://vitejs.dev/).
---
+++ src/components/App.tsx
@@ -0,0 +1,17 @@
+import Signature from "./Signature";
+
+const App = () => (
+  <div className="h-screen relative flex justify-center items-center ">
+    <img
+      className="absolute h-full w-full object-cover object-center lg:object-top"
+      src="/poet.min.png"
+      alt="The Poet"
+    />
+    <div className="absolute inset-0 h-full w-full bg-gradient-to-b from-[rgba(45,40,35,0.1)] to-[rgba(45,40,35,0.2)]"></div>
+    <Signature className="absolute top-[12vh] w-[72vw] max-w-[1200px] h-auto text-[#fad895]">
+      <title>Joseph De Martini</title>
+    </Signature>
+  </div>
+);
+
+export default App;
