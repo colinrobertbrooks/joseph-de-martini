3016884f5ec596cfca79870aa6407ebddf86ca0f
Colin Brooks <colinrobertbrooks@gmail.com>
2023-09-28 11:54:16 -0400

    fix: images

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
+import backgroundImg from "../assets/images/poet.min.png";
 import Signature from "./Signature";
 
 const App = () => (
   <div className="h-screen relative flex justify-center items-center ">
     <img
       className="absolute h-full w-full object-cover object-center lg:object-top"
-      src="/poet.min.png"
+      src={backgroundImg}
       alt="The Poet"
     />
     <div className="absolute inset-0 h-full w-full bg-gradient-to-b from-[rgba(45,40,35,0.1)] to-[rgba(45,40,35,0.2)]"></div>
